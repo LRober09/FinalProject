63d1104a38ea7a79c6a23146cceb2f825aa238f9
Lucas Robertson <lucasrobertson0@gmail.com>
2017-10-30 14:49:10 -0700

    Added basic navbar structure & styling

--- client/main.js
+++ client/main.js
@@ -2,5 +2,10 @@ import {Template} from 'meteor/templating';
 
 // HTML imports
 import './main.html';
+import '/imports/ui/Navbar/Navbar.html';
 
 // JS imports
+import '/imports/ui/Navbar/Navbar.js';
+
+// Style imports
+import "/imports/assets/sass/navbar.scss";
